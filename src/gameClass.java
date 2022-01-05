16915e3e5b3c7a313d705f847c73fd154a625055
revdoor <revdoor@kaist.ac.kr>
2022-01-05 20:34:39 +0900

    Card class

--- src/gameClass.java
+++ src/gameClass.java
@@ -1,2 +1,16 @@
+import javax.swing.*;
+
+class Card {
+    int color, number, shape, shadow;
+    int status;
+
+    Card(int color, int number, int shape, int shadow){
+        this.color = color;
+        this.number = number;
+        this.shape = shape;
+        this.shadow = shadow;
+    }
+}
+
 public class gameClass {  // file that includes all classes about game play
 }
