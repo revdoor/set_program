a45d14d4ddd0ec54753f1a66e300b6e804c2f0c5
revdoor <revdoor@kaist.ac.kr>
2022-01-06 04:25:32 +0900

    introduced IdentifierConstant for clean code

---
+++ src/gamePackage/IdentifierConstant.java
@@ -0,0 +1,23 @@
+package gamePackage;
+
+public final class IdentifierConstant {
+    public static final int COLOR_RED = 0;
+    public static final int COLOR_GREEN = 1;
+    public static final int COLOR_PURPLE = 2;
+
+    public static final int NUMBER_ONE = 0;
+    public static final int NUMBER_TWO = 1;
+    public static final int NUMBER_THREE = 2;
+
+    public static final int SHAPE_ROUND = 0;
+    public static final int SHAPE_DIAMOND = 1;
+    public static final int SHAPE_WAVE = 2;
+
+    public static final int SHADOW_EMPTY = 0;
+    public static final int SHADOW_LINE = 1;
+    public static final int SHADOW_FILL = 2;
+
+    public static final int STATUS_UNUSED = 0;
+    public static final int STATUS_ON_FIELD = 1;
+    public static final int STATUS_USED = 2;
+}
