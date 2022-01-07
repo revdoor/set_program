4d68d07bf9d6ddf55f71a14f49d78757c56d873e
mskim-17 <mskim17@unist.ac.kr>
2022-01-08 00:59:14 +0900

    add skeleton

--- src/gamePackage/gameClassTest.java
+++ src/gamePackage/gameClassTest.java
@@ -1,4 +1,18 @@
 package gamePackage;
 
+import java.util.*;
+
+class printObject {
+
+    void printGameField(GameField field) {
+
+    }
+
+    void printPlayerInfo(Player[] players) {
+
+    }
+
+}
+
 public class gameClassTest {
 }
